3b8e79cc03b3cc79abf032241d739339f0a8ba8b
DANI <DANI@DESKTOP-9UM1BJ1>
2024-03-15 13:47:50 +0100

    V1

---
+++ Carreras/src/datos/Localizacion.java
@@ -0,0 +1,9 @@
+package datos;
+
+public class Localizacion {
+
+	public static void main(String[] args) {
+		System.out.println("La localizacion de la carrera 4 es en Abu Dabi");
+		String carrera3 = "Barein";
+	}
+}
---
+++ Carreras/src/datos/Pilotos.java
@@ -0,0 +1,8 @@
+package datos;
+
+public class Pilotos {
+
+	public static void main(String[] args) {
+		System.out.println("Pilotos de la carrera 3, main Pablo Palardo");
+	}
+}
